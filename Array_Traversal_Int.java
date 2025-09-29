dd19ea438a8c4542d9732eac34319bf67f261c59
BABIN JOE <babinjoe2945@gmail.com>
2025-09-29 16:36:35 +0530

    Create Array_Traversal_Int.java

---
+++ Array_Traversal_Int.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class Array_Traversal_Int {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String[] ar = new String[5];
+        for(int i=0;i<5;i++){
+            ar[i] = sc.nextLine();
+        }
+        for(int i = 0;i<ar.length;i++){
+            System.out.println(ar[i]);
+        }
+    }
+}
