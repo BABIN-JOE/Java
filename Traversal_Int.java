d5ed9a8693e0d725e06010c4346e185d4ea46bc7
BABIN JOE <babinjoe2945@gmail.com>
2025-09-30 12:59:01 +0530

    Create Traversal_Int.java

---
+++ Traversal_Int.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+class Traversal_Int {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int temp = n;
+        int divisor = 1;
+        while (temp >= 10) {
+            temp = temp / 10;
+            divisor = divisor * 10;
+        }
+        while (divisor > 0) {
+            int digit = n / divisor;
+            System.out.println(digit);
+            n = n % divisor;
+            divisor = divisor / 10;
+        }
+    }
+}
