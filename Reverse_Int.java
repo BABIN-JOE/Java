5e48bc2c831f2b57718fe9fcd7c59598d077fe9d
BABIN JOE <babinjoe2945@gmail.com>
2025-09-30 12:48:39 +0530

    Create Reverse_Int.java

---
+++ Reverse_Int.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public record Reverse_Int() {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n  = sc.nextInt();
+        int rev = 0;
+        int temp;
+        while(n!=0){
+            temp = n%10;
+            rev = rev*10+temp;
+            n = n/10;
+        }
+        System.out.println(rev);
+    }
+}
