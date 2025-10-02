70b8b0a2e490a1fc492d23e3f09cdcb60794acb1
BABIN JOE <babinjoe2945@gmail.com>
2025-10-02 11:53:04 +0530

    Create TwoD_Array.java

---
+++ TwoD_Array.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class TwoD_Array {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int m = sc.nextInt();
+        int n = sc.nextInt();
+        int[][] arr = new int[m][n];
+        for(int i = 0;i<m;i++){
+            for(int j = 0;j<n;j++){
+                arr[i][j] = sc.nextInt();
+            }
+        }
+        for(int i = 0;i<m;i++){
+            for(int j = 0;j<n;j++){
+                System.out.print(arr[i][j]+"\t");
+            }
+            System.out.println();
+        }
+    }
+}
