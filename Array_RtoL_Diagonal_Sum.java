4ad309b9fba827978e04628386eb5cb89b284b09
BABIN JOE <babinjoe2945@gmail.com>
2025-10-02 11:54:38 +0530

    Create Array_RtoL_Diagonal_Sum.java

---
+++ Array_RtoL_Diagonal_Sum.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+public class Array_RtoL_Diagonal_Sum {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int m = sc.nextInt();
+        int n = sc.nextInt();
+        int[][] arr = new int[m][n];
+        for(int i = 0;i<m;i++){
+            for(int j = 0;j<n;j++){
+                arr[i][j] = sc.nextInt();
+            }
+        }
+        for(int i = 0;i<m;i++){
+            for(int j = 0;j<n;j++){
+                System.out.print(arr[i][j]+"\t");
+            }
+            System.out.println();
+        }
+        int sum = 0;
+        for(int i = 0;i<m;i++){
+            if(i<n){
+                sum = sum+ arr[i][n-1-i];
+            }
+        }
+        System.out.println(sum); 
+    }
+}
