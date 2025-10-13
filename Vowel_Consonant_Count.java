d189ce4e4ec460c75c35fa9de0f0ecea846cd0d0
BABIN JOE <babinjoe2945@gmail.com>
2025-10-13 16:42:21 +0530

    Create Vowel_Consonant_Count.java

---
+++ Vowel_Consonant_Count.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+public class Vowel_Consonant_Count {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+        s = s.replaceAll("\\s","").toLowerCase();
+        Set<Character> vowels = new HashSet<>(Arrays.asList('a','e','i','o','u'));
+        int c1 = 0;
+        int c2 = 0;
+        for(int i=0;i<s.length();i++){
+            char ch = s.charAt(i); 
+            if(ch >= 'a' && ch <= 'z'){
+                if(vowels.contains(ch)){
+                    c1++;
+                }
+                else{
+                    c2++;
+                }
+            }
+        }
+        System.out.println("Volwel Count : " +c1);
+        System.out.println("Consonant Count : " +c2);
+    }
+}
