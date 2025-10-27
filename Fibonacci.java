ff43d067d0879a6238a137ff6712d435dbc4139e
BABIN JOE <babinjoe2945@gmail.com>
2025-10-27 14:50:56 +0530

    Update Fibonacci.java

--- Fibonacci.java
+++ Fibonacci.java
@@ -1,1 +1,20 @@
 
+import java.util.*;
+
+public class Fibonacci {
+    public static void main(String[] Args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int a = 0;
+        int b = 1;
+        int c = 0;
+        System.out.println(a);
+        System.out.println(b);
+        for(int i = 0;i<n-2;i++){
+            c = a+b;
+            System.out.println(c);
+            a = b;
+            b = c;   
+        }
+    }
+}
