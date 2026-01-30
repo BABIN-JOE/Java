a0ee22d86ba66c2fe871f8c4bae0a621a11f0da2
BABIN JOE <babinjoe2945@gmail.com>
2026-01-30 13:43:09 +0530

    Update Factorial.java

--- Factorial.java
+++ Factorial.java
@@ -5,7 +5,7 @@ class Factorial{
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         int fact = 1;
-        for(int i =n;i>0;i--){
+        for(int i = n;i>0;i--){
             fact = fact*i;
         }
         System.out.println(fact);
